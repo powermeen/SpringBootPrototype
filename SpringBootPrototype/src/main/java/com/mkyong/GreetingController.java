24d9a74759c6399e7978355834dda6f4ce6bc0ca
powermeen <powermeen1@gmail.com>
2017-12-15 00:18:43 +0700

    join

---
+++ SpringBootPrototype/src/main/java/com/mkyong/GreetingController.java
@@ -0,0 +1,38 @@
+package com.mkyong;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+    @RequestMapping("/getString")
+    public String getString()
+    {
+    	Gson gson = new Gson();
+    	List<String > test = new ArrayList<>();
+    	
+    	
+    	test.add("12");
+    	test.add("13");
+    	test.add("14");
+    	test.add("15");
+    	gson.
+        return JSONObject.quote("Hello World");
+    }
+}
