b6c3472bffd2bd1fe2736e7083e4a11b98d701b9
powermeen <powermeen1@gmail.com>
2017-12-20 00:46:11 +0700

    create table

---
+++ SpringBootPrototype/src/main/resources/sql/PermissionTable.sql
@@ -0,0 +1,2 @@
+drop table if exists permission_table;
+CREATE TABLE permission_table ( i_permission_id INT(20) NOT NULL AUTO_INCREMENT , c_name VARCHAR(100) NOT NULL , c_detail VARCHAR(100) NOT NULL , PRIMARY KEY (i_permission_id));
